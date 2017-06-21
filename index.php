c9dfa9bb37d6a45dd0752c76e112099a698d69b5
steven <steven.coic@gmail.com>
2017-06-21 11:40:00 +0200

    'vignette'

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+include('inc/pdo.php');
+
+
+$sql = "SELECT id FROM movies_full ORDER BY RAND() LIMIT 10 ";
+$query = $pdo->prepare($sql);
+        $query->execute();
+        $movies = $query->fetchAll();
+
+foreach ($movies as $movie) {
+  $id = $movie['id'];
+  afficherImage($movie);
+}
+
+function afficherImage($movie) {
+  echo '<a href="detail.php?id= '.$movie['id'].'"><img class="vignette" src="inc/img/posters/'.$movie['id'].'.jpg"  />' ;
+}
+
+ ?>
